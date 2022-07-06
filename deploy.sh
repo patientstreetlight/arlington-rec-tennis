797670ae5e5da54d6488db64245ebe97d0a410e1
Chris Rice <chris@chrisrice.io>
2022-07-06 14:26:28 -0400

    Only have deploy.sh print the aws commands

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -x
-
 for file in $(find site -type f) ; do
     base_file=$(echo $file | sed 's,^site/,,')
+    set -x
     aws s3 cp ${file} s3://chrisrice.io/${base_file}
+    set +x
 done
